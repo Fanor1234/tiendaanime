bbc83a81a28bc58bb4015f7ac083f6c61e8f9250
Fanor1234 <63176500+Fanor1234@users.noreply.github.com>
2023-12-22 12:46:26 -0400

    Update settings.py

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-6k*9to4cslv-%139=-k8!$e9*(toer989-(lyks8=mf2!tl_u9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
+ALLOWED_HOSTS = ['.vercel.app']
 #ALLOWED_HOSTS = ["animate-production.up.railway.app"]
 
 
