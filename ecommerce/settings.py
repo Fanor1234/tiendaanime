fb2736ac65a229ab5c5ee11f5cd2c4f0160f6f89
Fanor1234 <luis_faurp@hotmail.com>
2023-12-25 13:47:46 -0400

    static

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -132,7 +132,7 @@ USE_TZ = True
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 STATIC_URL = "static/"
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
